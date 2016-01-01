7c20d79877307bd98436914de9f9517204f99966
M1cha <sigmaepsilon92@gmail.com>
2016-01-01 20:54:34 +0100

    add icon for CWM

--- EFIDroidUi.c
+++ EFIDroidUi.c
@@ -293,6 +293,10 @@ GetAndroidImgInfo (
       *Icon = libaroma_stream_ramdisk("icons/recovery_twrp.png");
       *ImgName = AsciiStrDup("TWRP");
     }
+    else if (CpioGetByName(Ramdisk, "res/images/icon_clockwork.png")) {
+      *Icon = libaroma_stream_ramdisk("icons/recovery_clockwork.png");
+      *ImgName = AsciiStrDup("ClockworkMod Recovery");
+    }
     else {
       *Icon = libaroma_stream_ramdisk("icons/android.png");
       *ImgName = AsciiStrDup("Recovery");
