2508ffca1f0ba655cda92ab8dacf27e917c55363
M1cha <sigmaepsilon92@gmail.com>
2016-02-02 07:40:03 +0100

    FastbootCommands: add support for 'fastboot boot'

--- FastbootCommands.c
+++ FastbootCommands.c
@@ -11,10 +11,22 @@ CommandReboot (
   gRT->ResetSystem (EfiResetCold, EFI_SUCCESS, 0, NULL);
 }
 
+STATIC VOID
+CommandBoot (
+  CHAR8 *Arg,
+  VOID *Data,
+  UINT32 Size
+)
+{
+  FastbootOkay("");
+  AndroidBootFromBuffer(Data, Size, NULL, TRUE);
+}
+
 VOID
 FastbootCommandsAdd (
   VOID
 )
 {
   FastbootRegister("reboot", CommandReboot);
+  FastbootRegister("boot", CommandBoot);
 }
