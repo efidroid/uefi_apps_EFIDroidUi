0cc20faac42a8f25c666e64062b2315ee47ed722
M1cha <sigmaepsilon92@gmail.com>
2016-02-07 18:04:43 +0100

    Library: AromaLib: fb_driver: add support for bgr888

--- Library/AromaLib/fb_driver.c
+++ Library/AromaLib/fb_driver.c
@@ -70,11 +70,11 @@ void UEFIFBDR_setrgbpos(LIBAROMA_FBP me, byte r, byte g, byte b) {
 }
 
 /*
- * Function    : UEFIFBDR_post
+ * Function    : UEFIFBDR_post_bgra8888
  * Return Value: byte
  * Descriptions: post data
  */
-byte UEFIFBDR_post(
+byte UEFIFBDR_post_bgra8888(
   LIBAROMA_FBP me, wordp __restrict src,
   int dx, int dy, int dw, int dh,
   int sx, int sy, int sw, int sh
@@ -96,6 +96,36 @@ byte UEFIFBDR_post(
     dstride, 
     sstride,
     mi->rgb_pos
+  );
+  return 1;
+}
+
+/*
+ * Function    : UEFIFBDR_post_bgr888
+ * Return Value: byte
+ * Descriptions: post data
+ */
+byte UEFIFBDR_post_bgr888(
+  LIBAROMA_FBP me, wordp __restrict src,
+  int dx, int dy, int dw, int dh,
+  int sx, int sy, int sw, int sh
+  ){
+  if (me == NULL) {
+    return 0;
+  }
+  UEFIFBDR_INTERNALP mi = (UEFIFBDR_INTERNALP) me->internal;
+  int sstride = (sw - dw) * 2;
+  int dstride = (mi->line - (dw * mi->pixsz));
+  bytep copy_dst =
+    (bytep) (((bytep) mi->buffer)+(mi->line * dy)+(dx * mi->pixsz));
+  wordp copy_src =
+    (wordp) (src + (sw * sy) + sx);
+  libaroma_blt_align24(
+    copy_dst,
+    copy_src,
+    dw, dh,
+    dstride,
+    sstride
   );
   return 1;
 }
@@ -161,20 +191,41 @@ byte UEFIFBDR_init(LIBAROMA_FBP me) {
   
   /* set internal useful data */
   mi->buffer    = (void*)(UINTN)mi->gop->Mode->FrameBufferBase;
-  mi->depth     = 32;
+
+  EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* ModeInfo = mi->gop->Mode->Info;
+  switch(ModeInfo->PixelFormat) {
+    case PixelBlueGreenRedReserved8BitPerColor:
+      me->post        = &UEFIFBDR_post_bgra8888;
+      mi->depth     = 32;
+      UEFIFBDR_setrgbpos(me,16,8,0);
+      break;
+
+    case PixelRedGreenBlueReserved8BitPerColor:
+      me->post        = &UEFIFBDR_post_bgra8888;
+      mi->depth     = 32;
+      UEFIFBDR_setrgbpos(me,0,8,16);
+      break;
+
+    // just assume bgr888 for now
+    case PixelBitMask:
+      me->post        = &UEFIFBDR_post_bgr888;
+      mi->depth     = 24;
+      break;
+
+    default:
+      ASSERT(FALSE);
+      goto error;
+  }
   mi->pixsz     = mi->depth >> 3;
   mi->line      = me->w * mi->pixsz;
   mi->fb_sz     = (me->w * me->h * mi->pixsz);
   
   /* swap buffer now */
   UEFIFBDR_flush(me);
-
-  UEFIFBDR_setrgbpos(me,16,8,0);
  
   mi->stride = mi->line - (me->w * mi->pixsz);
   me->start_post  = &UEFIFBDR_start_post;
   me->end_post    = &UEFIFBDR_end_post;
-  me->post        = &UEFIFBDR_post;
   me->snapshoot   = &UEFIFBDR_snapshot;
 
   me->dpi = mi->lk_display->GetDensity(mi->lk_display);
