89daa72981cd2c40e3713d3339ed37ba33b8b104
M1cha <sigmaepsilon92@gmail.com>
2016-02-15 09:25:36 +0100

    remove wrong license headers

--- EFIDroidUi.h
+++ EFIDroidUi.h
@@ -1,17 +1,3 @@
-/** @file
-*
-*  Copyright (c) 2011-2015, ARM Limited. All rights reserved.
-*
-*  This program and the accompanying materials
-*  are licensed and made available under the terms and conditions of the BSD License
-*  which accompanies this distribution.  The full text of the license may be found at
-*  http://opensource.org/licenses/bsd-license.php
-*
-*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-*
-**/
-
 #ifndef __EFIDROIDUI_H__
 #define __EFIDROIDUI_H__
 
